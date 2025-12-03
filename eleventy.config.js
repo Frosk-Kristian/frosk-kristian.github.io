5b06118eb33969720c90b7e4feb3bff0333a196b
Kristian Frossos <55474756+Frosk-Kristian@users.noreply.github.com>
2025-12-03 14:42:45 +0800

    Update eleventy.config.js
    copy css/ to _site/css

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,7 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+	// copy `css/` to `_site/css/`
+	eleventyConfig.addPassthroughCopy("css");
 };
